ef608f462940ba27012745a814f7ff6c4072dbd7
aplkaev <almakaev-2011@mail.ru>
2022-05-02 12:45:37 +0500

    комментарии и вынес функции счета людей в комнате

--- backend/chat_copy/ChatBack/chat/consumers.py
+++ backend/chat_copy/ChatBack/chat/consumers.py
@@ -4,59 +4,135 @@ import json
 
 
 class ChatConsumer(WebsocketConsumer):
-
+    my_rooms = {}
     def connect(self):
-        
-        # print('\n\t\t',(self.scope),'\n')
+        """
+            Действие при подключение wss клиентом (рукопожатие)
+
+            Создаем группу и добавляем туда человека
+            Сообщаем тем кто уже есть, сколько теперь людей в комнате
+        """
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-        # print('connect',self.room_name, self.room_group_name, self.channel_name, self.channel_layer)
+
+        # создаем группу и добавляем человека в группу
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
 
         self.accept()
+        self._add_person_room()
+
 
     def disconnect(self, close_code):
-        print('disconnect',self.room_name, self.room_group_name)
+        """
+            Отключение пользователя
+        """
+        if self.room_group_name in self.my_rooms:
+            self.my_rooms[self.room_group_name] -= 1
+
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
+        self._rm_person_room()
+
 
     def receive(self, text_data):
+        """
+            Сообщение от пользователя
+        """
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        data = {
+            'type': 'chat_message',
+            'message': message,
+        }
+        
         if message.strip() == '':
-            return
+            data['type'] = 'chat_error'
+            data['message'] = 'Ошибка. Пустое сообщение'
+
+        self._send_message(data)
 
 
-        print(text_data, message)
-
-        print('receive',self.room_name, self.room_group_name)
+    def _add_person_room(self):
+        """
+            Добавляем человека в счетчик людей в комнате
+        """
+        # для определения количества людей в группе
+        if not self.room_group_name in self.my_rooms:
+            self.my_rooms[self.room_group_name] = 0
+        self.my_rooms[self.room_group_name] += 1
+        data = {
+            'type':'chat_len',
+            'len':self.my_rooms[self.room_group_name]
+        }
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message,
-                # 'username': username
-            }
+            data
         )
-        # self.chat_message({
-        #         'type': 'chat_message',
-        #         'message': message,
-        #         # 'username': username
-        #     })
-        
+
+    
+    def _rm_person_room(self):
+        """
+            Убираем человека из комнаты
+        """
+        data = {
+            'type':'chat_len',
+            'len':self.my_rooms[self.room_group_name]
+        }
+        self._send_message(data)
+
+
+    def _send_message(self, data):
+        """
+            Отправляем сообщение
+        """
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            data
+        )
+
+
+    def chat_error(self, event:dict):
+        """
+            Оповещение об ошибки
+
+            Например пустой сообщение
+            TODO: добавить предупреждение через код ошибки
+
+            event:dict - {
+                'message':'Тест ошибки'
+            }
+        """
+        self.send(text_data=json.dumps({
+            'event': "error",
+            'message': event['message'],
+        }))      
+
 
     def chat_message(self, event):
+        """
+            Сообщением все пользователем в комнате
+        """
         print('chat_message',event)
         message = event['message']
-        # username = event['username']
         
         self.send(text_data=json.dumps({
-            'event': "Send",
+            'event': "send",
             'message': message,
-            # 'username': username
         }))
+
+
+    def chat_len(self, event):
+        """
+            Пишем всем сколько людей в комнате
+        """
+        print(event)
+        self.send(text_data=json.dumps({
+            'event': "users_len",
+            'len': event['len'],
+        }))
+
