500b63392f25027f7905332efd0ac001ee5b2786
aplkaev <almakaev-2011@mail.ru>
2022-05-08 15:32:14 +0500

    include axios и bootstap

--- amplify-datastore/src/main.js
+++ amplify-datastore/src/main.js
@@ -1,28 +1,58 @@
 import Vue from 'vue'
 import App from './App.vue'
-import socket from './utils/socket'
 import Notifications from 'vue-notification'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+
+import socket from './utils/socket'
 import store from './store'
+import './assets/style.css';
+
+/**
+ * https://bootstrap-vue.org/docs
+ */
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+/**
+ * https://bootstrap-vue.org/docs
+ */
+
+
+/**
+ * 
+ * https://www.npmjs.com/package/vue-axios
+ * 
+ */
+Vue.use(VueAxios, axios);
+/**
+ * 
+ * https://www.npmjs.com/package/vue-axios
+ * 
+ */
+
+
+
+// import VueSocketIO from 'vue-socket.io'
+// TODO:внедрить SOCKETIO
 // import VueSocketIO from 'vue-socket.io'
 
-Vue.config.productionTip = false
-Vue.prototype.socket = socket
+Vue.config.productionTip = false;
+Vue.prototype.socket = socket;
 
-
-// Vue.use(new VueSocketIO({
-//   debug: true,
-//   connection: 'http://metinseylan.com:1992',
-//   vuex: {
-//       store,
-//       actionPrefix: 'SOCKET_',
-//       mutationPrefix: 'SOCKET_'
-//   },
-//   options: { path: "/my-app/" } //Optional options
-// }))
-Vue.use(Notifications)
+Vue.use(Notifications);
 
 
 new Vue({
+  socket,
   store,
   render: h => h(App),
 }).$mount('#app')
