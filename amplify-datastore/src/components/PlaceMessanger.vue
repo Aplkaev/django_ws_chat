b490e640c20aefdcb3d185ac0e6a0fd16d73798f
aplkaev <almakaev-2011@mail.ru>
2022-05-08 15:33:42 +0500

    Отображать разные components от условия введения названия комнаты

--- amplify-datastore/src/components/PlaceMessanger.vue
+++ amplify-datastore/src/components/PlaceMessanger.vue
@@ -1,14 +1,18 @@
 <template>
     <div>
-        <div class="place__conteiner">
-            <div class="place__border">
+        <div class="place__conteiner p-4">
+            <div class="place__input_room" v-if="hasRoomName === undefined || hasRoomName === null">
+                <RoomPlace />
+            </div>
+            <div v-else class="place__border">
                 <MessagesView />
                 <InputMessanger />
+                <div class="user_len">
+                    Количество людей в комнате: {{ lenUsersRoom }}
+                </div>
             </div>
         </div>
-        <div class="user_len">
-            Количество людей в комнате: {{ lenUsersRoom }}
-        </div>
+
     </div>
 </template>
 <script>
@@ -17,60 +21,23 @@
     } from 'vuex'
     import MessagesView from './MessagesView.vue';
     import InputMessanger from './InputMessanger.vue';
+    import RoomPlace from './RoomPlace.vue';
     export default ({
         name: 'PlaceMessanger',
         components: {
             MessagesView,
-            InputMessanger
+            InputMessanger,
+            RoomPlace
         },
-        // computed: {
-        //     lenUsersRoom(state) {
-        //         return this.$store.getters.lenUsersRoom;
-        //     }
-        // },
-        computed:mapGetters(['lenUsersRoom']),
+        computed: mapGetters(['lenUsersRoom', 'hasRoomName']),
         data: function () {
             return {
                 users_len: 0,
-                methods: {
-                    // users_len: (data) => {
-                    //     this.users_len = data.len;
-                    // }
-                }
+                methods: {}
             }
         },
         mounted() {
 
-        }
+        },
     })
 </script>
-
-
-<style scoped>
-    .place__conteiner {
-        width: 100vw;
-        height: 100vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
-
-    .place__border {
-        /* border: 1px solid; */
-        display: flex;
-        flex-direction: column;
-        height: 90%;
-        width: 100%;
-        max-width: 800px;
-        backdrop-filter: #000;
-        -webkit-box-shadow: 3px 4px 32px -14px rgba(34, 60, 80, 0.2) inset;
-        -moz-box-shadow: 3px 4px 32px -14px rgba(34, 60, 80, 0.2) inset;
-        box-shadow: 3px 4px 32px -14px rgba(34, 60, 80, 0.2) inset;
-    }
-
-    .user_len {
-        position: absolute;
-        left: 10px;
-        top: 10px;
-    }
-</style>
