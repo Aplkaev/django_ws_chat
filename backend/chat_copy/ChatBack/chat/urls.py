38dfbac97de26ec821fdb0ffbfc651b0f5e45d9d
aplkaev <almakaev-2011@mail.ru>
2022-05-11 22:33:13 +0500

    add api getRooms and getMyId

--- backend/chat_copy/ChatBack/chat/urls.py
+++ backend/chat_copy/ChatBack/chat/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('api/getRooms/', views.get_rooms),
+    path('api/getMyId/', views.get_my_id),
     path('<str:room_name>/', views.room, name='room'),
+    path('', views.index, name='index'),
 ]
