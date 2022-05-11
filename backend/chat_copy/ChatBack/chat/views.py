4a7c400eef5d98c5be97c19c806efedfa6e2a911
aplkaev <almakaev-2011@mail.ru>
2022-05-11 22:32:57 +0500

    Получение всех комнат и id для чата

--- backend/chat_copy/ChatBack/chat/views.py
+++ backend/chat_copy/ChatBack/chat/views.py
@@ -1,7 +1,11 @@
 import json
+import time
 
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
+from django.http import JsonResponse
+from .consumers import ChatConsumer 
+
 
 
 def index(request):
@@ -13,4 +17,30 @@ def room(request, room_name):
     """"""
     return render(request, 'chat/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
+    })
+
+
+def get_rooms(request):
+    """Запрос на получение всех комнат"""
+    rooms = []
+    c = ChatConsumer()
+    my_rooms = c.my_rooms
+    c_keys = my_rooms.keys()
+    for i in c_keys:
+        rooms.append({
+            'name':i,
+            'users_len':my_rooms[i]
+        })
+    return JsonResponse({
+        'items':rooms
+    })
+
+
+def get_my_id(request):
+    """
+        Выдаем пользователю уникальный id
+    """
+
+    return JsonResponse({
+        'id':int(time.time()*1000)
     })
